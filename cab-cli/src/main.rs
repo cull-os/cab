5ab3ad418b1dfe0fb819f2b02746ad1e674118b3
RGBCube <git@rgbcu.be>
2024-06-08 16:12:15 +0300

    Imitate clap's errors with env_logger

--- cab-cli/src/main.rs
+++ cab-cli/src/main.rs
@@ -1,13 +1,30 @@
+use std::{
+    io::{
+        self,
+        Write,
+    },
+    process,
+};
+
 use cab_syntax::Tokenizer;
 use clap::{
     Parser,
     Subcommand,
 };
 use clap_stdin::FileOrStdin;
+use clap_verbosity_flag::{
+    InfoLevel,
+    Verbosity,
+};
+use colored::Colorize;
+use log::Level;
 
 #[derive(Parser)]
 #[command(name = "cab", version, about)]
 struct Cli {
+    #[command(flatten)]
+    verbosity: Verbosity<InfoLevel>,
+
     #[command(subcommand)]
     command: Command,
 }
@@ -30,17 +47,45 @@ enum Command {
 }
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() -> io::Result<()> {
     let cli = Cli::parse();
+
+    // Trying to imitate clap to get a consistent experience.
+    env_logger::Builder::new()
+        .filter_level(cli.verbosity.log_level_filter())
+        .format(|buffer, record| {
+            let level = match record.level() {
+                Level::Error => "error:".red().bold(),
+                Level::Warn => "warn:".yellow().bold(),
+                Level::Info => "info:".green().bold(),
+                Level::Debug => "debug:".blue().bold(),
+                Level::Trace => "trace:".cyan().bold(),
+            };
+
+            writeln!(buffer, "{level} {arguments}", arguments = record.args())
+        })
+        .init();
+
+    let mut out = io::BufWriter::new(io::stdout());
 
     match cli.command {
         Command::TokenDump { file } => {
-            Tokenizer::new(&file.contents()?)
-                .into_iter()
-                .for_each(|token| println!("{kind:?} {slice:?}", kind = token.0, slice = token.1));
+            let expression = file.contents().unwrap_or_else(|error| {
+                log::error!("failed to read file: {error}");
+                process::exit(1);
+            });
+
+            for token in Tokenizer::new(&expression) {
+                writeln!(out, "{kind:?} {slice:?}", kind = token.0, slice = token.1)
+                    .unwrap_or_else(|error| {
+                        log::error!("failed to write to stdout: {error}");
+                        process::exit(1);
+                    });
+            }
         },
         Command::AstDump { .. } => {
-            todo!();
+            log::error!("not implemented yet");
+            process::exit(1);
         },
     }
 
