b769d35d85555ece4c7aeb5fe7fb64f4c6ea5c2b
RGBCube <git@rgbcu.be>
2024-06-08 14:52:17 +0300

    Accept a file as input for token-dump

--- cab-cli/src/main.rs
+++ cab-cli/src/main.rs
@@ -1,3 +1,8 @@
+use std::{
+    fs,
+    path::Path,
+};
+
 use cab_syntax::Tokenizer;
 use clap::{
     Parser,
@@ -14,11 +19,11 @@ struct Cli {
 #[derive(Subcommand)]
 enum Command {
     /// Dump the provided expressions tokens.
-    TokenDump { expression: String },
+    TokenDump { expression_or_file: String },
     /// Dump the provided expression's abstract syntax tree
     /// in the form of an unambigious Cab expression that is
     /// very similar to Lisp.
-    AstDump { expression: String },
+    AstDump { expression_or_file: String },
 }
 
 #[tokio::main]
@@ -26,14 +31,18 @@ async fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
 
     match cli.command {
-        Command::TokenDump { expression } => {
+        Command::TokenDump { expression_or_file } => {
+            let expression = if Path::new(&expression_or_file).is_file() {
+                fs::read_to_string(expression_or_file)?
+            } else {
+                expression_or_file
+            };
+
             Tokenizer::new(&expression)
                 .into_iter()
                 .for_each(|token| println!("{kind:?} {slice:?}", kind = token.0, slice = token.1));
         },
-        Command::AstDump {
-            expression: _expression,
-        } => {
+        Command::AstDump { .. } => {
             todo!();
         },
     }
