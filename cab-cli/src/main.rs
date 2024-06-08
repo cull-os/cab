dce42ac2a80ebda794a053c42ce200ae1ba2e027
RGBCube <git@rgbcu.be>
2024-06-08 15:09:49 +0300

    Use clap_stdin

--- cab-cli/src/main.rs
+++ cab-cli/src/main.rs
@@ -1,13 +1,9 @@
-use std::{
-    fs,
-    path::Path,
-};
-
 use cab_syntax::Tokenizer;
 use clap::{
     Parser,
     Subcommand,
 };
+use clap_stdin::FileOrStdin;
 
 #[derive(Parser)]
 #[command(name = "cab", version, about)]
@@ -18,12 +14,19 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Command {
-    /// Dump the provided expressions tokens.
-    TokenDump { expression_or_file: String },
-    /// Dump the provided expression's abstract syntax tree
-    /// in the form of an unambigious Cab expression that is
-    /// very similar to Lisp.
-    AstDump { expression_or_file: String },
+    /// Dump the provided file.
+    TokenDump {
+        #[clap(default_value = "-")]
+        file: FileOrStdin,
+    },
+
+    /// Dump the provided file's abstract syntax tree
+    /// in the form of an unambigious Cab expression
+    /// that is very similar to Lisp.
+    AstDump {
+        #[clap(default_value = "-")]
+        file: FileOrStdin,
+    },
 }
 
 #[tokio::main]
@@ -31,14 +34,8 @@ async fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
 
     match cli.command {
-        Command::TokenDump { expression_or_file } => {
-            let expression = if Path::new(&expression_or_file).is_file() {
-                fs::read_to_string(expression_or_file)?
-            } else {
-                expression_or_file
-            };
-
-            Tokenizer::new(&expression)
+        Command::TokenDump { file } => {
+            Tokenizer::new(&file.contents()?)
                 .into_iter()
                 .for_each(|token| println!("{kind:?} {slice:?}", kind = token.0, slice = token.1));
         },
