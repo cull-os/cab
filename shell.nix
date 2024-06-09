39714a2816da671c0de7b988cb139300ed4b30be
RGBCube <git@rgbcu.be>
2024-06-09 10:31:03 +0000

    Add target folders to PATH in shell.nix

--- shell.nix
+++ shell.nix
@@ -8,6 +8,9 @@ mkShell {
   LD_LIBRARY_PATH = "${stdenv.cc.cc.lib}/lib";
 
   shellHook = ''
+    root=$(git rev-parse --show-toplevel 2>/dev/null || echo ".")
+    export PATH=$PATH:$root/target/debug:$root/target/release
+
     if command -v crash >/dev/null 2>&1; then
       crash
       exit $?
